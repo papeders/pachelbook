659345f822d1315761aeed9b864f4739b16f7c2e
Pachel A Pederson <oreb197@upcorp.ad.uprr.com>
2021-05-14 11:31:55 -0500

    users

---
+++ models/users.js
@@ -0,0 +1,12 @@
+const users = [
+    {
+        username: "pachelp",
+        email: "p@p.com",
+        password:"123",
+        aboutMe: " Pachel Pederson is Pachel Pederson!",
+        img: "https://upload.wikimedia.org/wikipedia/en/thumb/8/81/Sarah_Connor_%28Linda_Hamilton%29.jpg/220px-Sarah_Connor_%28Linda_Hamilton%29.jpg",
+        uuid: 12345
+    },
+]
+
+module.exports = users;
